24ab6135c29f8d5d8b30c406a14ce11462658cb1
Crease84 <91694954+Crease84@users.noreply.github.com>
2021-11-03 23:21:37 -0600

    Update TBG.cpp

--- TBG.cpp
+++ TBG.cpp
@@ -1,137 +1,136 @@
-//▓█████▄  ▒█████    ▄████    ▄▄▄█████▓ ▒█████   ▒█████  ▄▄▄█████▓ ██░ ██
-//▒██▀ ██▌▒██▒  ██▒ ██▒ ▀█▒   ▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓  ██▒ ▓▒▓██░ ██▒
-//░██   █▌▒██░  ██▒▒██░▄▄▄░   ▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒ ▓██░ ▒░▒██▀▀██░
-//░▓█▄   ▌▒██   ██░░▓█  ██▓   ░ ▓██▓ ░ ▒██   ██░▒██   ██░░ ▓██▓ ░ ░▓█ ░██
-//░▒████▓ ░ ████▓▒░░▒▓███▀▒     ▒██▒ ░ ░ ████▓▒░░ ████▓▒░  ▒██▒ ░ ░▓█▒░██▓
-//▒▒▓  ▒ ░ ▒░▒░▒░  ░▒   ▒      ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░   ▒ ░░    ▒ ░░▒░▒
-//░ ▒  ▒   ░ ▒ ▒░   ░   ░        ░      ░ ▒ ▒░   ░ ▒ ▒░     ░     ▒ ░▒░ ░
-//░ ░  ░ ░ ░ ░ ▒  ░ ░   ░      ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░       ░  ░░ ░
-//░        ░ ░        ░                 ░ ░      ░ ░            ░  ░  ░
-//░
-
- //    _____          ___           ___                                         ___           ___                       ___     
- //   /  /::\        /  /\         /  /\                            ___        /  /\         /  /\          ___        /__/\    
- //  /  /:/\:\      /  /::\       /  /:/_                          /  /\      /  /::\       /  /::\        /  /\       \  \:\   
- // /  /:/  \:\    /  /:/\:\     /  /:/ /\                        /  /:/     /  /:/\:\     /  /:/\:\      /  /:/        \__\:\  
- ///__/:/ \__\:|  /  /:/  \:\   /  /:/_/::\                      /  /:/     /  /:/  \:\   /  /:/  \:\    /  /:/     ___ /  /::\ 
- //\  \:\ /  /:/ /__/:/ \__\:\ /__/:/__\/\:\                    /  /::\    /__/:/ \__\:\ /__/:/ \__\:\  /  /::\    /__/\  /:/\:\
- // \  \:\  /:/  \  \:\ /  /:/ \  \:\ /~~/:/                   /__/:/\:\   \  \:\ /  /:/ \  \:\ /  /:/ /__/:/\:\   \  \:\/:/__\/
- //  \  \:\/:/    \  \:\  /:/   \  \:\  /:/                    \__\/  \:\   \  \:\  /:/   \  \:\  /:/  \__\/  \:\   \  \::/     
- //   \  \::/      \  \:\/:/     \  \:\/:/                          \  \:\   \  \:\/:/     \  \:\/:/        \  \:\   \  \:\     
- //    \__\/        \  \::/       \  \::/                            \__\/    \  \::/       \  \::/          \__\/    \  \:\    
- //                  \__\/         \__\/                                       \__\/         \__\/                     \__\/    
-
 #include<iostream>
-#include<iostream>
+#include<Windows.h>
 using namespace std;
+
+void logo() {
+	system("color 4");
+	cout << "################################################################################################################################" << endl;
+	cout << "#    _____          ___           ___                                         ___           ___                       ___      #" << endl;
+	cout << "#   /  /  \\        /  /\\         /  /\\                            ___        /  /\\         /  /\\          ___        /__/\\     #" << endl;
+	cout << "#  /  / /\\ \\      /  /  \\       /  / /_                          /  /\\      /  /  \\       /  /  \\        /  /\\       \\  \\ \\    #" << endl;
+	cout << "# /  / /  \\ \\    /  / /\\ \\     /  / / /\\                        /  / /     /  / /\\ \\     /  / /\\ \\      /  / /        \\__\\ \\   #" << endl;
+	cout << "#/__/ / \\__\\ |  /  / /  \\ \\   /  / /_/  \\                      /  / /     /  / /  \\ \\   /  / /  \\ \\    /  / /     ___ /  /  \\  #" << endl;
+	cout << "#\\  \\ \\ /  / / /__/ / \\__\\ \\ /__/ /__\\/\\ \\                    /  /  \\    /__/ / \\__\\ \\ /__/ / \\__\\ \\  /  /  \\    /__/\\  / /\\ \\ #" << endl;
+	cout << "# \\  \\ \\  / /  \\  \\ \\ /  / / \\  \\ \\ /~~/ /                   /__/ /\\ \\   \\  \\ \\ /  / / \\  \\ \\ /  / / /__/ /\\ \\   \\  \\ \\/ /__\\/ #" << endl;
+	cout << "#  \\  \\ \\/ /    \\  \\ \\  / /   \\  \\ \\  / /                    \\__\\/  \\ \\   \\  \\ \\  / /   \\  \\ \\  / /  \\__\\/  \\ \\   \\  \\  /      #" << endl;
+	cout << "#   \\  \\  /      \\  \\ \\/ /     \\  \\ \\/ /                          \\  \\ \\   \\  \\ \\/ /     \\  \\ \\/ /        \\  \\ \\   \\  \\ \\      #" << endl;
+	cout << "#    \\__\\/        \\  \\  /       \\  \\  /                            \\__\\/    \\  \\  /       \\  \\  /          \\__\\/    \\  \\ \\     #" << endl;
+	cout << "#                  \\__\\/         \\__\\/                                       \\__\\/         \\__\\/                     \\__\\/     #" << endl;
+	cout << "################################################################################################################################" << endl;
+}
+
 int main() {
 
-	string Inventory[5]{"Flashlight"};
-	int room = 1;  
-	string input;
+	logo();
 
-	cout << "You looking like an astronust that like goes to space or whatever ON A MISSION but, The space ship you are on EXPLODEDSSSSSSS" << endl;
-	cout << "You floating there in space when a HUGE space HOUSE hits you and you find away inside" << endl;
-	cout << "Youre gonna be straned there for a while, Better go explore or DIE of loneliness" << endl;
-	cout << "-Enter 'q' to exit and any point" << endl;
-	cout << "GAME START" << endl;
+	system("color B");
+	string inventory[5]{ "flashlight" };
+	int room = 1;
+	char input;
+
+	cout << endl;
+	cout << "you looking like an aStronust that like goes To space or whAtever on a mission but, the space ship You are on explodedsssssss" << endl;
+	cout << "While floating around spacE a transdImensional home ship appeaRs from the emptyness, You are able to finD a way aboard" << endl;
+	cout << "youre gonna be straned there for a while, better go explore or die of loneliness  " << endl;
+	cout << "enter 'q' to exit at any point" << endl;
 	cout << endl;
 
 	do {
 		switch (room) {
 		case 1:
 
-		 
-			cout << "Its pretty DARK but, using your flashlight, the room looks like a kitchen" << endl;
-			cout << "There's a stove, dishwasher, fridge, and complementing blue countertops" << endl;
-			cout << "but Spilling out of the darkness, the room is full of DOLLS! DOLLS EVERYWHERE!!!" << endl;
-			cout << "falling out of the fridge, Hanging from the ceiling, Pinned to the walls" << endl;
-			cout << "They all seems to trck you with their eyes" << endl;
-			cout << "Theres a door that leads North and another that goes to the East" << endl;
+			cout << "its pretty dark but, using your flashlight, the room looks like a kitchen" << endl;
+			cout << "there's a stove, dishwasher, fridge, and complementing blue countertops" << endl;
+			cout << "but spilling out of the darkness, the room is full of dolls! dolls everywhere!!!  " << endl;
+			cout << "falling out of the fridge, hanging from the ceiling, pinned to the walls" << endl;
+			cout << "they all seems to trck you with their eyes" << endl;
+			cout << "theres a door that leads north and another that goes to the east" << endl;
+			
 
-			
 			cin >> input;
-			
-			if (input == 'E')
+
+			if (input == 'e')
 				room = 2;
-			else if (input == 'N')
-				room = 4
+			if (input == 'n')
+				room = 4;
 
-				cout << "DOES IT LOOK LIKE YOU CAN GO THAT WAY?" << endl;
+			cout << "Im not sure you can go that way" << endl;
 			break;
-		
+
 		case 2:
-			cout << " The cavern is covered in greenish liquid that seeps from the walls" << endl;
-			cout << " Theres a big rock blocking then exit to the north but you can go in any OTHER direction" << endl;
-			
+			cout << "A coutch and coffe table accent the broken futureistic tv that lights up the room" << endl;
+			cout << "This is probably the living room" << endl;
+			cout << "Theres a key inside a coffee mug thats on the table" << endl;
+			cout << "Spiders are occupying the coutch, looks like thier favorte show is on...IDK" << endl;
+			cout << "You can see a door to the West and a hole in the wall to the North" << endl;
+
 			cin >> input;
 
-			if (input == 'S')
-				room = 1;
-			if (input == 'W')
+			if (input == 'N')
 				room = 3;
-			if (input == 'E')
-				room = 4;
+			if (input == 'W')
+				room = 1;
 			else
-				cout << "Which direction is that?" << endl;
+				cout << "which direction is that?" << endl;
 			break;
 
 		case 3:
-			cout << "Theres a skeleton on the floor..SPOOPY" << endl;
-			cout << "The exit is to the east" << endl;
+			cout << "Theres toilet! AND ITS USED" << endl;
+			cout << "My favorite part is the snakes comming out of the sink" << endl;
+			cout << "Theres a door theres a door just ajar to the West and the big hole to the South" << endl;
 
 			cin >> input;
 
 
-			if (input == 'E')
+			if (input == 'W')
+				room = 4;
+			if (input == 'S')
 				room = 2;
 			else
-				cout << "The skelly bro waved at you" << endl;
+				cout << "But nobody came..." << endl;
 			break;
 
 		case 4:
-			cout << "You better be quick, the roof look like it can cave in ANY MOMENT" << endl;
-			cout << " You can move to the North or West" << endl;
+			cout << "A big bed takes up most of the room" << endl;
+			cout <<"would be comfy if it wasnt covered in maggots and cockroches" << endl;
+			cout << "Watch your step, theres some glass on the foor" << endl;
+			cout << "Your exits are to the West, South, and East" << endl;
 
 			cin >> input;
 
-			if (input == 'N')
+			if (input == 'W')
 				room = 5;
-			else if (input == 'W')
-				room = 2;
+			if (input == 'S')
+				room = 1;
+			else if (input == 'E')
+				room = 3;
 			else
-				cout << "crazy = genuis" << endl;
+				cout << "Error - 404" << endl;
 			break;
 
 		case 5:
-			cout << "for some reason the room is BRIGHT WHITE" << endl;
-			cout << "Theres and exit to the North and South" << endl;
+			cout << "Nothing much going on here" << endl;
+			cout << "A grand stair case spirals downward?" << endl;
+			cout << "Im not sure how thats possible?" << endl;
+			cout << "You can go West down the stairs or easyt to the bedroom" << endl;
 
 			cin >> input;
-				
-			if (input == 'N')
+
+			if (input == 'W')
 				room = 6;
-			else if (input == 'S')
+			else if (input == 'E')
 				room = 4;
 			else
-				cout << "MMM rock flavored ROCKS" << endl;
+				cout << "Yea, NO... " << endl;
 			break;
-				
+
 		case 6:
-			cout << "The cave has Rocks everywhere and its alittle wet..sorta feels familiar?" << endl;
-			cout << "Theres an exit to the North, Not one to the South" << endl;
+			cout << "You make your way down the steps carefully" << endl;
+			cout << "Half way down something grabs your ankle" << endl; 
+			cout << "Your flashlight hits every step on the way down as your body follows its lead to the bottom" << endl;
 
-			cin >> input;
-
-			if (input == 'N')
-				room = 2;
-			if (input == 'S')
-				cout << "theres no Exit to the South...." << endl;
-			else
-				cout << "IT HURTS MY HEADDDDD" << endl;
 			break;
 
 		}
 	} while (input != 'q');
-
 }
