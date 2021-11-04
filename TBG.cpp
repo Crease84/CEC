58596051f0e747498bf652d289f0992a99d68a0f
Crease84 <91694954+Crease84@users.noreply.github.com>
2021-11-04 14:50:51 -0600

    Update TBG.cpp

--- TBG.cpp
+++ TBG.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 void logo() {
-	system("color 4");
+	/*system("color 4");
+	Sleep(400)
+	cout << "####################################################################" << endl;
+	cout << "#    _____          ___           ___                              #" << endl;
+	cout << "#   /  /  \\        /  /\\         /  /\\                          #" << endl;
+	cout << "#  /  / /\\ \\      /  /  \\       /  / /_                         #" << endl;
+	cout << "# /  / /  \\ \\    /  / /\\ \\     /  / / /\\                      #" << endl;
+	cout << "#\\  \\ \\ /  / / /__/ / \\__\\ \\ /__/ /__\\/\\ \\                #" << endl;
+	cout << "# \\  \\ \\  / /  \\  \\ \\ /  / / \\  \\ \\ /~~/ /                #" << endl;
+	cout << "#  \\  \\ \\/ /    \\  \\ \\  / /   \\  \\ \\  / /                 #" << endl;
+	cout << "#   \\  \\  /      \\  \\ \\/ /     \\  \\ \\/ /                   #" << endl;
+	cout << "#    \\__\\/        \\  \\  /       \\  \\  /                      #" << endl;
+	cout << "#                  \\__\\/         \\__\\/                         #" << endl;
+	cout << "####################################################################" << endl;
+	Sleep(200);
+	system("CLS");
 	cout << "################################################################################################################################" << endl;
 	cout << "#    _____          ___           ___                                         ___           ___                       ___      #" << endl;
 	cout << "#   /  /  \\        /  /\\         /  /\\                            ___        /  /\\         /  /\\          ___        /__/\\     #" << endl;
@@ -17,35 +32,41 @@ void logo() {
 	cout << "#    \\__\\/        \\  \\  /       \\  \\  /                            \\__\\/    \\  \\  /       \\  \\  /          \\__\\/    \\  \\ \\     #" << endl;
 	cout << "#                  \\__\\/         \\__\\/                                       \\__\\/         \\__\\/                     \\__\\/     #" << endl;
 	cout << "################################################################################################################################" << endl;
+	system("pause");
+	system("CLS");*/
 }
 
 int main() {
-
+	
 	logo();
 
 	system("color B");
-	string inventory[5]{ "flashlight" };
+	string inventory[5]{ "flashlight"};
 	int room = 1;
 	char input;
 
-	cout << endl;
 	cout << "you looking like an aStronust that like goes To space or whAtever on a mission but, the space ship You are on explodedsssssss" << endl;
 	cout << "While floating around spacE a transdImensional home ship appeaRs from the emptyness, You are able to finD a way aboard" << endl;
 	cout << "youre gonna be straned there for a while, better go explore or die of loneliness  " << endl;
-	cout << "enter 'q' to exit at any point" << endl;
+	cout << "You have";
+	cout << " a " << inventory[0] << " ";
 	cout << endl;
+	cout << "ENTER 'q' TO EXIT AT ANY TIME" << endl;
+	cout << endl;
+	system("pause");
+	system("CLS");
 
 	do {
 		switch (room) {
 		case 1:
+			system("CLS");
+			cout << "Its pretty dark but,  the room looks like a kitchen" << endl;
+			cout << "There's a stove, dishwasher, fridge, and complementing blue countertops" << endl;
+			cout << "But spilling out of the darkness, the room is full of dolls! dolls everywhere!!!  " << endl;
+			cout << "Falling out of the fridge, hanging from the ceiling, pinned to the walls" << endl;
+			cout << "They all seems to trck you with their eyes" << endl;
+			cout << "Theres a door that leads north and another that goes to the east" << endl;
 
-			cout << "its pretty dark but, using your flashlight, the room looks like a kitchen" << endl;
-			cout << "there's a stove, dishwasher, fridge, and complementing blue countertops" << endl;
-			cout << "but spilling out of the darkness, the room is full of dolls! dolls everywhere!!!  " << endl;
-			cout << "falling out of the fridge, hanging from the ceiling, pinned to the walls" << endl;
-			cout << "they all seems to trck you with their eyes" << endl;
-			cout << "theres a door that leads north and another that goes to the east" << endl;
-			
 
 			cin >> input;
 
@@ -58,9 +79,10 @@ int main() {
 			break;
 
 		case 2:
-			cout << "A coutch and coffe table accent the broken futureistic tv that lights up the room" << endl;
+			system("CLS");
+			cout << "A coutch and an accent coffe table are flipped over and theres a futureistic tv that lights up the room with an EERE blue light" << endl;
 			cout << "This is probably the living room" << endl;
-			cout << "Theres a key inside a coffee mug thats on the table" << endl;
+			cout << "Theres a key inside a coffee mug thats on the floor by the table" << endl;
 			cout << "Spiders are occupying the coutch, looks like thier favorte show is on...IDK" << endl;
 			cout << "You can see a door to the West and a hole in the wall to the North" << endl;
 
@@ -75,6 +97,7 @@ int main() {
 			break;
 
 		case 3:
+			system("CLS");
 			cout << "Theres toilet! AND ITS USED" << endl;
 			cout << "My favorite part is the snakes comming out of the sink" << endl;
 			cout << "Theres a door theres a door just ajar to the West and the big hole to the South" << endl;
@@ -91,8 +114,9 @@ int main() {
 			break;
 
 		case 4:
+			system("CLS");
 			cout << "A big bed takes up most of the room" << endl;
-			cout <<"would be comfy if it wasnt covered in maggots and cockroches" << endl;
+			cout << "would be comfy if it wasnt covered in maggots and cockroches" << endl;
 			cout << "Watch your step, theres some glass on the foor" << endl;
 			cout << "Your exits are to the West, South, and East" << endl;
 
@@ -109,6 +133,7 @@ int main() {
 			break;
 
 		case 5:
+			system("CLS");
 			cout << "Nothing much going on here" << endl;
 			cout << "A grand stair case spirals downward?" << endl;
 			cout << "Im not sure how thats possible?" << endl;
@@ -124,9 +149,11 @@ int main() {
 				cout << "Yea, NO... " << endl;
 			break;
 
+			
 		case 6:
+			system("CLS");
 			cout << "You make your way down the steps carefully" << endl;
-			cout << "Half way down something grabs your ankle" << endl; 
+			cout << "Half way down something grabs your ankle" << endl;
 			cout << "Your flashlight hits every step on the way down as your body follows its lead to the bottom" << endl;
 
 			break;
