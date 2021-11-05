513f38b4e2079eb172b8bb29968a3288df135eda
Crease84 <91694954+Crease84@users.noreply.github.com>
2021-11-04 22:15:01 -0600

    Update TBG.cpp

--- TBG.cpp
+++ TBG.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
 #include<Windows.h>
+#include<vector>
+#include<string>
 using namespace std;
 
+vector<string> inventory(5);
+
 void logo() {
-	/*system("color 4");
-	Sleep(400)
+	system("color 4");
+	system("pause");
+	Sleep(400);
+	cout << "################################################" << endl;
+	cout << "#    _____          __                     #" << endl;
+	cout << "#   /  /  \\        /                       #" << endl;
+	cout << "#  /  / /\\ \\      /  /                    #" << endl;
+	cout << "# /  / /  \\ \\    /  / /\\ \\             #" << endl;
+	cout << "#\\  \\ \\ /  / / /__/ / \\             #" << endl;
+	cout << "# \\  \\ \\  / /  \\  \\               #" << endl;
+	cout << "#  \\  \\ \\/ /    \\             #" << endl;
+	cout << "#   \\  \\  /      \\                  #" << endl;
+	cout << "#    \\__\\/        \\                   #" << endl;
+	cout << "#                  \\__                #" << endl;
+	cout << "#################################################" << endl;
+	Sleep(200);
+	system("CLS");
 	cout << "####################################################################" << endl;
 	cout << "#    _____          ___           ___                              #" << endl;
 	cout << "#   /  /  \\        /  /\\         /  /\\                          #" << endl;
@@ -17,6 +36,20 @@ void logo() {
 	cout << "#    \\__\\/        \\  \\  /       \\  \\  /                      #" << endl;
 	cout << "#                  \\__\\/         \\__\\/                         #" << endl;
 	cout << "####################################################################" << endl;
+	Sleep(200);
+	system("CLS");
+	cout << "###########################################################################################################################" << endl;
+	cout << "#    _____          ___           ___                                         ___           ___                       ___      #" << endl;
+	cout << "#   /  /  \\        /  /\\         /  /\\                            ___        /  /\\         /  /\\      _/   #" << endl;
+	cout << "#  /  / /\\ \\      /  /  \\       /  / /                         /  /\\      /  /  \\        \\    \\ \\    #" << endl;
+	cout << "# /  / /  \\ \\    /  / /\\ \\     /  / / /\\                        /  / /     /  / /\\ \\     /  / /\\ \\      /  /   #" << endl;
+	cout << "#/__/ / \\__\\ |  /  / /  \\ \\   /  / /_/  \\                      /  / /     /  / /  \\ \\  \\_ / \\ #" << endl;
+	cout << "# \\  \\ \\  / /  \\  \\ \\ /  / / \\  \\ \\ /~~/ /                   /__/ /\\ \\   \\  \\ \\ /  / / \\  \\ \\ /  / / /___\\/ #" << endl;
+	cout << "#  \\  \\ \\/ /    \\  \\ \\  / /   \\  \\ \\  / /                    \\__\\/  \\ \\   \\  \\ \\  / /   \\  \\ \\  / /  \\  \\  /      #" << endl;
+	cout << "#   \\  \\  /      \\  \\ \\/ /     \\  \\ \\/ /                          \\  \\ \\   \\  \\ \\/ /     \\   \\ \\      #" << endl;
+	cout << "#    \\__\\/        \\  \\  /       \\  \\  /                            \\__\\/    \\  \\  /       \\  \\   \\     #" << endl;
+	cout << "#                  \\__\\/         \\__\\/                                       \\__\\/         \\__\\/     \\/     #" << endl;
+	cout << "###########################################################################################################################" << endl;
 	Sleep(200);
 	system("CLS");
 	cout << "################################################################################################################################" << endl;
@@ -33,23 +66,26 @@ void logo() {
 	cout << "#                  \\__\\/         \\__\\/                                       \\__\\/         \\__\\/                     \\__\\/     #" << endl;
 	cout << "################################################################################################################################" << endl;
 	system("pause");
-	system("CLS");*/
+	system("CLS");
+}
+void Inventory() {
+	cout << "Currenly, you are holding: " << endl;
+	for (int i = 5; i > 0; i--)
+		cout << "- " << inventory[i] << endl;
 }
 
 int main() {
-	
-	logo();
+	//logo();
+	inventory.push_back("Flashlight");
 
 	system("color B");
-	string inventory[5]{ "flashlight"};
 	int room = 1;
-	char input;
+	string input;
 
 	cout << "you looking like an aStronust that like goes To space or whAtever on a mission but, the space ship You are on explodedsssssss" << endl;
 	cout << "While floating around spacE a transdImensional home ship appeaRs from the emptyness, You are able to finD a way aboard" << endl;
 	cout << "youre gonna be straned there for a while, better go explore or die of loneliness  " << endl;
-	cout << "You have";
-	cout << " a " << inventory[0] << " ";
+	cout << "good thing you brought a flashlight" << endl;
 	cout << endl;
 	cout << "ENTER 'q' TO EXIT AT ANY TIME" << endl;
 	cout << endl;
@@ -60,6 +96,7 @@ int main() {
 		switch (room) {
 		case 1:
 			system("CLS");
+			cout << ">" << input << endl;
 			cout << "Its pretty dark but,  the room looks like a kitchen" << endl;
 			cout << "There's a stove, dishwasher, fridge, and complementing blue countertops" << endl;
 			cout << "But spilling out of the darkness, the room is full of dolls! dolls everywhere!!!  " << endl;
@@ -67,91 +104,127 @@ int main() {
 			cout << "They all seems to trck you with their eyes" << endl;
 			cout << "Theres a door that leads north and another that goes to the east" << endl;
 
+			getline(cin, input);
 
-			cin >> input;
-
-			if (input == 'e')
+			if (input == "Inventory") {
+				system("CLS");
+				Inventory();
+				system("pause");
+			}
+			else if (input == "Go East")
 				room = 2;
-			if (input == 'n')
+			else if (input == "Go North")
 				room = 4;
-
-			cout << "Im not sure you can go that way" << endl;
+			else {
+				cout << "Im not sure you can go that way" << endl;
+			}
 			break;
 
 		case 2:
-			system("CLS");
+			//system("CLS");
+			cout << ">" << input << endl;
 			cout << "A coutch and an accent coffe table are flipped over and theres a futureistic tv that lights up the room with an EERE blue light" << endl;
 			cout << "This is probably the living room" << endl;
-			cout << "Theres a key inside a coffee mug thats on the floor by the table" << endl;
+			if (inventory[1] != "Key")
+				cout << "Theres a key inside a coffee mug thats on the floor by the table" << endl;
+			else
 			cout << "Spiders are occupying the coutch, looks like thier favorte show is on...IDK" << endl;
 			cout << "You can see a door to the West and a hole in the wall to the North" << endl;
 
-			cin >> input;
+			getline(cin, input);
 
-			if (input == 'N')
+			
+			if (input == "Take Key") {
+				cout << "You added the Key to your inventory" << endl;
+				inventory.push_back("Key");
+			}
+			else if (input == "Inventory") {
+				system("CLS");
+				Inventory();
+				system("pause");
+			}
+			else if (input == "Go North")
 				room = 3;
-			if (input == 'W')
+			else if (input == "Go West")
 				room = 1;
 			else
-				cout << "which direction is that?" << endl;
+				cout << "You WHAT?" << endl;
 			break;
 
 		case 3:
-			system("CLS");
+			//system("CLS");
+			cout << ">" << input << endl;
 			cout << "Theres toilet! AND ITS USED" << endl;
 			cout << "My favorite part is the snakes comming out of the sink" << endl;
 			cout << "Theres a door theres a door just ajar to the West and the big hole to the South" << endl;
 
-			cin >> input;
+			getline(cin, input);
 
-
-			if (input == 'W')
+			if (input == "Inventory") {
+				system("CLS");
+				Inventory();
+				system("pause");
+			}
+			if (input == "Go West")
 				room = 4;
-			if (input == 'S')
+			if (input == "Go South")
 				room = 2;
 			else
 				cout << "But nobody came..." << endl;
 			break;
 
 		case 4:
-			system("CLS");
+			//system("CLS");
+			cout << ">" << input << endl;
 			cout << "A big bed takes up most of the room" << endl;
 			cout << "would be comfy if it wasnt covered in maggots and cockroches" << endl;
 			cout << "Watch your step, theres some glass on the foor" << endl;
-			cout << "Your exits are to the West, South, and East" << endl;
+			cout << "Theres a closed door to the west, Other exits include: South, and East" << endl;
 
-			cin >> input;
+			getline(cin, input);
 
-			if (input == 'W')
-				room = 5;
-			if (input == 'S')
+			if (input == "Invt" || "Inventory")
+				Inventory();
+			if (input == "Go West")
+				if (inventory[1] == "Key") {
+					cout << "You Insert the key and turn the lock" << endl;
+					cout << "You are able to push open the heavy door" << endl;
+				}
+				else{
+					cout << "Doesnt look like you have a key to open the door" << endl;
+				}
+			if (input == "Go South")
 				room = 1;
-			else if (input == 'E')
+			else if (input == "Go East")
 				room = 3;
 			else
 				cout << "Error - 404" << endl;
 			break;
 
 		case 5:
-			system("CLS");
+			//system("CLS");
+			cout << ">" << input << endl;
 			cout << "Nothing much going on here" << endl;
 			cout << "A grand stair case spirals downward?" << endl;
 			cout << "Im not sure how thats possible?" << endl;
 			cout << "You can go West down the stairs or easyt to the bedroom" << endl;
 
-			cin >> input;
+			getline(cin, input);
 
-			if (input == 'W')
+			if (input == "Invt" || "Inventory")
+				Inventory();
+			if (input == "Go West")
 				room = 6;
-			else if (input == 'E')
+			else if (input == "Go East")
 				room = 4;
 			else
 				cout << "Yea, NO... " << endl;
 			break;
 
-			
+
 		case 6:
-			system("CLS");
+			//system("CLS");
+			cout << ">" << input << endl;
 			cout << "You make your way down the steps carefully" << endl;
 			cout << "Half way down something grabs your ankle" << endl;
 			cout << "Your flashlight hits every step on the way down as your body follows its lead to the bottom" << endl;
@@ -159,5 +232,5 @@ int main() {
 			break;
 
 		}
-	} while (input != 'q');
+	} while (input != "q");
 }
